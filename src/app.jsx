e0f737b0bbc7f30d7b2f9292c5b889a4b20188bc
Marcelo Iván González Henríquez <marcelo.gonzalez.14@sansano.usm.cl>
2018-04-20 01:59:59 -0300

    Se modifica index.html

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
+      <h2>Proyecto IngSW 2018-1</h2>
+      <h2>La media de ... es: </h2>
     </div>);
   }
 }
